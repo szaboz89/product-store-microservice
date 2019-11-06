bc062a9acf56b8f649837a1be920a0eedf94a939
Zoltán Szabó <szaboz89@gmail.com>
2019-11-06 09:17:23 +0100

    Init

---
+++ src/main/java/com/szabodev/example/spring/product/store/microservice/ProductStoreMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.szabodev.example.spring.product.store.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStoreMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductStoreMicroserviceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/szabodev/example/spring/product/store/microservice/ProductStoreMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szabodev.example.spring.product.store.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStoreMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
